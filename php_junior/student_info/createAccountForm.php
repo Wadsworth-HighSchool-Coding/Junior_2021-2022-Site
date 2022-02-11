b8ff1bb1f80ee473f8901c916147d977c0f22a68
TANK1_41 <bradenaey1@gmail.com>
2022-02-11 08:58:13 -0500

    added captcha to create account and login page

--- php_junior/student_info/createAccountForm.php
+++ php_junior/student_info/createAccountForm.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -31,6 +34,12 @@
             }
         }
     </script>
+    <?php
+    create_image();
+    display();
+    function display()
+    {
+    ?>
 </head>
 <body>
 <div class="content">
@@ -45,13 +54,65 @@
             <input type="password" id="password" name="password"></p>
         <p><label for="password1">Password again:</label>
             <input type="password" id="password1" name="password1"></p>
-
+        <p>
+            <img src="captcha_image.png" style="border: thin solid;">
+        </p>
+        <p>
+            <label for="captcha">Captcha:</label>
+            <input type="text" name="captcha" id="captcha">
+        </p>
 
         <p><input type="submit" name="submit" id="submit" value="submit"></p>
         <?php
+        }
+
+        function create_image()
+        {
+            $image = imagecreatetruecolor(200, 50);
+            $background_color = imagecolorallocate($image, 255, 255, 255);
+            imagefilledrectangle($image, 0, 0, 200, 50, $background_color);
+
+            $line_color = imagecolorallocate($image, 245, 66, 90);
+            $number_of_lines = rand(3, 7);
+
+            for ($i = 0; $i < $number_of_lines; $i++) {
+                imageline($image, 0, rand() % 50, 250, rand() % 50, $line_color);
+            }
+
+            $pixel = imagecolorallocate($image, 0, 0, 255);
+            for ($i = 0; $i < 500; $i++) {
+                imagesetpixel($image, rand() % 200, rand() % 50, $pixel);
+            }
+
+            $allowed_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789';
+            $length = strlen($allowed_letters);
+            $letter = $allowed_letters[rand(0, $length - 1)];
+            $word = '';
+            $text_color = imagecolorallocate($image, 0, 0, 0);
+            $cap_length = 6;// No. of character in image
+            for ($i = 0; $i < $cap_length; $i++) {
+                $letter = $allowed_letters[rand(0, $length - 1)];
+                imagestring($image, 5, 5 + ($i * 30), 20, $letter, $text_color);
+                $word .= $letter;
+            }
+
+            $_SESSION['captcha_string'] = $word;
+
+            imagepng($image, "captcha_image.png");
+
+        }
+
+        ?>
+        <?php
         if (isset($_GET["error"])) {
+            if ($_GET["error"] == "wrongCaptcha") {
+                echo "<h2> Wrong captcha please try again</h2>";
+            }
             if ($_GET["error"] == "emptyInput") {
                 echo "<h2> Error please try again</h2>";
+            }
+            if ($_GET["error"] == "noCaptchaData") {
+                echo "<h2> Error no captcha data please try again</h2>";
             }
         }
         ?>
--- php_junior/student_info/loginForm.php
+++ php_junior/student_info/loginForm.php
@@ -1,3 +1,4 @@
+<?php session_start() ?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -39,9 +40,18 @@
             case "PleaseLogIn":
                 echo "<h2>Please login before visiting other pages</h2>";
                 break;
-
+            case "badCaptcha":
+                echo "<h2>incorrect captcha try again</h2>";
+                break;
+            case "noCaptchaData":
+                echo "<h2>Error No captchaData</h2>";
+                break;
         }
     }
+    create_image();
+    display();
+    function display()
+    {
     ?>
     <form type="submit" method="post" name="form1" action="validateUser.php" onsubmit="return checkBlanks()">
         <p>
@@ -54,10 +64,58 @@
             <label for="password">password: </label>
             <input type="password" id="password" name="password">
         </p>
+        <p>
+            <img src="captcha_image.png" style="border: thin solid;">
+        </p>
+        <p>
+            <label for="captcha">Captcha:</label>
+            <input type="text" name="captcha" id="captcha">
+        </p>
+        <p>
 
-        <input type="submit" name="submit" id="submit" value="submit">
+            <input type="submit" name="submit" id="submit" value="submit">
 
     </form>
 </div>
 </body>
+<?php
+}
+
+function create_image()
+{
+    $image = imagecreatetruecolor(200, 50);
+    $background_color = imagecolorallocate($image, 255, 255, 255);
+    imagefilledrectangle($image, 0, 0, 200, 50, $background_color);
+
+    $line_color = imagecolorallocate($image, 245, 66, 90);
+    $number_of_lines = rand(3, 7);
+
+    for ($i = 0; $i < $number_of_lines; $i++) {
+        imageline($image, 0, rand() % 50, 250, rand() % 50, $line_color);
+    }
+
+    $pixel = imagecolorallocate($image, 0, 0, 255);
+    for ($i = 0; $i < 500; $i++) {
+        imagesetpixel($image, rand() % 200, rand() % 50, $pixel);
+    }
+
+    $allowed_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ23456789';
+    $length = strlen($allowed_letters);
+    $letter = $allowed_letters[rand(0, $length - 1)];
+    $word = '';
+    $text_color = imagecolorallocate($image, 0, 0, 0);
+    $cap_length = 6;// No. of character in image
+    for ($i = 0; $i < $cap_length; $i++) {
+        $letter = $allowed_letters[rand(0, $length - 1)];
+        imagestring($image, 5, 5 + ($i * 30), 20, $letter, $text_color);
+        $word .= $letter;
+    }
+
+    $_SESSION['captcha_string'] = $word;
+
+    imagepng($image, "captcha_image.png");
+
+}
+
+?>
 </html>
